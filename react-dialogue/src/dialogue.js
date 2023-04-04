f56d0199e0532971ceee5e8011c9949473d1dbb0
n.tungluntony@wustl.edu <n.tungluntony@wustl.edu>
2023-04-03 19:28:15 -0500

    App skeleton

---
+++ react-dialogue/src/dialogue.js
@@ -0,0 +1,12 @@
+// React STT Tutorial: https://www.loginradius.com/blog/engineering/quick-look-at-react-speech-recognition/
+
+import React, { useEffect, useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+
+
+export const Dialogue = () => {
+    return(
+        <div></div>
+    );
+};
